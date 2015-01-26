d43435d0713512ce5e5a750ffa551cce04bc335c
Trung Nguyen <trungnd10@thinclient-02.isg.directv.com>
2015-01-26 15:07:50 -0800

    test 2nd

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var http = require('http');
 
-var content = "<!DOCTYPE html><html xmlns=\"http://www.w3.org/1999/xhtml\"><title>ABC</title><body>Nguyễn Đức Trung</body></html>";
+var content = "Nguyễn Đức Trung";
+
+content += " xin chao";
+
+var document = "<!DOCTYPE html><html xmlns=\"http://www.w3.org/1999/xhtml\"><title>ABC</title><body>" + content + "</body></html>";
 
 http.createServer(function (request, response) {
     //response.writeHead(200, {'Content-Type': 'text/plain'});
     response.writeHead(200, {'Content-Type': 'application/xhtml+xml; charset=utf-8'});
-    response.end(content+'\n');
+    response.end(document+'\n');
 }).listen(8080);
 
 console.log('Server started');
